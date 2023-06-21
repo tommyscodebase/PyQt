6d00bd8d45ce58a8a006f1b32729e0b9df0b2ba5
Tommy Burns <tbbotchwey01@gmail.com>
2023-06-21 04:47:14 +0000

    creating thumbnail and adding image details

--- Image_Converter/main_class.py
+++ Image_Converter/main_class.py
@@ -1,7 +1,9 @@
 import os.path
 
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from main import Ui_ImaCon
+from PIL import Image
 
 
 class ImageConverter(QMainWindow, Ui_ImaCon):
@@ -17,6 +19,9 @@ class ImageConverter(QMainWindow, Ui_ImaCon):
         self.actionAdd_Images.triggered.connect(self.add_images)
         self.actionRemove_Selected.triggered.connect(self.remove_selected)
         self.actionRemove_All.triggered.connect(self.remove_all)
+        self.listWidget.itemClicked.connect(
+            lambda: self.create_thumbnail(self.images[self.listWidget.currentRow()])
+        )
 
     # Add Images
     def add_images(self):
@@ -55,3 +60,42 @@ class ImageConverter(QMainWindow, Ui_ImaCon):
                     self.images.clear()
         except Exception as e:
             print(f"Removing Images Error: {e}")
+
+    # Image Thumbnail
+    def create_thumbnail(self, image):
+        try:
+            self.thumbnail_label.setPixmap(QtGui.QPixmap(image))
+            self.image_details()
+        except Exception as e:
+            print(f"Creating thumbnail error: {e}")
+
+    # Image Details
+    def image_details(self):
+        try:
+            index = self.listWidget.currentRow()
+            image = self.images[index]
+
+            file_size = os.path.getsize(image)
+
+            # Convert file size to KB or MB
+            if file_size < 1024:
+                size = f"{round(file_size, 2)} bytes"
+            elif file_size < 1024 * 1024:
+                size = f"{round(file_size / 1024, 2)} KB"
+            else:
+                size = f"{round(file_size / (1024 * 1024), 2)} MB"
+
+            # Image dimensions
+            img = Image.open(image)
+            x, y = img.size
+            self.img_details_textEdit.clear()
+            self.img_details_textEdit.insertPlainText(
+                f"Name: {os.path.basename(image)}\n"
+                f"Location: {os.path.dirname(image)}\n"
+                f"Size: {size}\n"
+                f"Width: {x} pixels\n"
+                f"Height: {y} pixels".strip()
+
+            )
+        except Exception as e:
+            print(f"Image details error: {e}")
